e9bac2c2d7f986f982b45094cf8d07817e50b6c6
yuu-eguci <yuu.eguci@gmail.com>
2021-10-04 20:14:44 +0900

    🚚 views.py を刷新

--- app/views.py
+++ app/views.py
@@ -1,12 +1,18 @@
+# Built-in modules.
+import os
+
+# Third-party modules.
+
+# User modules.
+from app.utils import prediction_utils
+
 # from django.http import request
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.http.response import JsonResponse
-import json
-# from django.views.decorators.csrf import ensure_csrf_cookie
+from django.http.response import JsonResponse, HttpResponseBadRequest
 from django.views.generic import View
-
-# from django.views.generic import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 
 
 # NOTE:app/index.htmlが呼び出されるリターンされる。
@@ -14,28 +20,51 @@ def index(request):
     return HttpResponse('top')
 
 
-# NOTE:'api/vl/boot'が呼び出されるとjsonを返す。クラス
-class BootView(View):
+class StatusView(View):
     def get(self, request):
-        data = {
-            'test_massage': 'Is this right(Boot)'
-        }
+        # 手元に hdf5 が存在することを ready=true と解釈します。
+        data = dict(
+            ready=os.path.exists(settings.APP_HDF5_PATH_IN_APP),
+        )
         return JsonResponse(data=data)
 
 
-# NOTE:'api/vl/verify-pincode'が呼び出されるとjsonを返す。クラス
-class Verify_pincode(View):
-    def get(self, request):
-        data = {
-            'test_massage': 'Is this right(Verify_pincode)'
-        }
+@method_decorator(csrf_exempt, name='dispatch')
+class VerifyPincodeView(View):
+    def post(self, request):
+        # 必須パラメータをチェックします。 pincode がなければ BadRequest です。
+        if 'pincode' not in request.POST:
+            return HttpResponseBadRequest('pincode is required.')
+
+        # pincode をチェックします。
+        data = dict(
+            verification_succeeded=request.POST['pincode'] == settings.APP_PINCODE,
+        )
         return JsonResponse(data=data)
 
 
-# NOTE:'api/vl/prodict-image'が呼び出されるとjsonを返す。クラス
-class Predict_image(View):
-    def get(self, request):
-        data = {
-            'test_massage': 'Is this right(Prodict_image)'
-        }
+@method_decorator(csrf_exempt, name='dispatch')
+class PredictImageView(View):
+    def post(self, request):
+        # 必須パラメータをチェックします。必須パラメータがなければ BadRequest です。
+        if 'base64image' not in request.POST:
+            return HttpResponseBadRequest('base64image is required.')
+
+        # Prediction を行います。
+        prediction = prediction_utils.predict_base64image(request.POST['base64image'])
+
+        # Prediction の結果を、 json 用に整形します。
+        def for_json(tuple_: tuple) -> dict:
+            # ('Sunflower', 0.9995204)
+            # -> {'name':'Sunflower', 'confidence'=0.9995204} 変換します。
+            name = tuple_[0]
+            # json は float32 を serialize しません。 str にしてしまいます。
+            confidence = str(tuple_[1])
+            return dict(name=name, confidence=confidence)
+        result = list(map(lambda tuple_: for_json(tuple_), prediction))
+
+        data = dict(
+            hdf5_version=settings.APP_HDF5_VERSION,
+            result=result,
+        )
         return JsonResponse(data=data)
